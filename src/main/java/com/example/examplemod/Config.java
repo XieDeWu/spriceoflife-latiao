f7c0abbb109c7068c61da83ee72834fcfec5b15a
XieDeWu <3331732340@qq.com>
2025-09-04 20:09:55 +0800

    添加了默认饱食度饱和度公式草稿

--- src/main/java/com/example/examplemod/Config.java
+++ src/main/java/com/example/examplemod/Config.java
@@ -17,26 +17,31 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    public static final ModConfigSpec.ConfigValue<Integer> HISTORY_LENGTH = BUILDER
-            .comment("Number of eaten foods that will affect food values of next item eaten")
-            .defineInRange("historyLength", 20, 1, 1000);
+    public static final ModConfigSpec.ConfigValue<Integer> HISTORY_LENGTH_LONG = BUILDER
+            .comment("长期食用历史长度,用于统计长期饮食数据")
+            .defineInRange("historyLengthLong", 512, 0, 2048);
 
-    public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    public static final ModConfigSpec.ConfigValue<Integer> HISTORY_LENGTH_RECENT = BUILDER
+            .comment("短期食用历史长度,用于统计短期饮食数据")
+            .defineInRange("historyLengthRecent", 16, 0, 2048);
 
-    public static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+    public static final ModConfigSpec.ConfigValue<String> LOSS = BUILDER
+            .comment("默认流失公式,取决于饥饿程度、近期饮食,饮食清淡程度 || 相关变量 ")
+            .define("loss", "0.002*Math.pow(???HUNGER_LEVEL+SATURATION_LEVEL,???)*Math.log(???HUNGER_RECENT???)");
 
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
+    public static final ModConfigSpec.ConfigValue<String> HUNGER = BUILDER
+        .comment("默认饱食度公式,取决于近期饮食、长期饮食、饥饿程度 || 相关变量 该食物饱食度 HUNGER 该食物近期食用次数 EATEN_RECENT 玩家饱食度 HUNGER_LEVEL 该食物长期食用次数 EATEN_LONG")
+        .define("hunger", "HUNGER*0.4*Math.max(Math.pow(0.7,EATEN_RECENT),Math.max(1-HUNGER_LEVEL/10,0))+HUNGER*0.4*Math.max(1-EATEN_LONG/64,0)+HUNGER*0.2");
+
+    public static final ModConfigSpec.ConfigValue<String> SATURATION = BUILDER
+        .comment("默认饱和度公式,取决于近期饮食、长期饮食、饥饿程度 || 相关变量 该食物饱和度 SATURATION 该食物近期食用次数 EATEN_RECENT 该食物长期食用次数 EATEN_LONG 玩家饱食度 HUNGER_LEVEL")
+        .define("saturation", "SATURATION*Math.pow(0.7,EATEN_RECENT)*Math.max(1.0-EATEN_LONG/64.0,0.0)+HUNGER*0.3*Math.max(1.0-HUNGER_LEVEL/10.0,0.0)");
 
     // a list of strings that are treated as resource locations for items
     public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
+
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -92,11 +92,11 @@ public class ExampleMod {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
-        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-        }
+//        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+//            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+//        }
 
-        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
+//        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
 
         Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
