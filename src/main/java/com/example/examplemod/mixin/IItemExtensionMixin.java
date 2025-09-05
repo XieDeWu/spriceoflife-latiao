d0bd783c7b566a268d857b5408651fef522d3bc7
XieDeWu <3331732340@qq.com>
2025-09-05 23:11:41 +0800

    添加新旧Food组件映射测试，渲染与数值正常，下步计划安装变换公式

--- src/main/java/com/example/examplemod/mixin/IItemExtensionMixin.java
+++ src/main/java/com/example/examplemod/mixin/IItemExtensionMixin.java
@@ -1,5 +1,6 @@
 package com.example.examplemod.mixin;
 
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
@@ -9,11 +10,23 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mixin(IItemExtension.class)
 public interface IItemExtensionMixin {
     @Overwrite
     @Nullable // read javadoc to find a potential problem
     default FoodProperties getFoodProperties(ItemStack stack, @Nullable LivingEntity entity) {
-        return stack.get(DataComponents.FOOD);
+        var food = stack.get(DataComponents.FOOD);
+        if(food == null) return food;
+        int nutrition = Math.min(food.nutrition() + 10,20);
+        float saturation = Math.min(food.saturation() + 10,20);
+        boolean canAlwaysEat = food.canAlwaysEat();
+        float eatSeconds = food.eatSeconds();
+        Optional<ItemStack> usingConvertsTo = food.usingConvertsTo();
+        List<FoodProperties.PossibleEffect> effects = food.effects();
+        FoodProperties modifyFood = new FoodProperties(nutrition,saturation,canAlwaysEat,eatSeconds,usingConvertsTo,effects);
+        return modifyFood;
     }
 }
