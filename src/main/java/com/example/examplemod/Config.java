2be5f18276ca94644906be47518f43bb6d33f465
XieDeWu <3331732340@qq.com>
2025-09-05 09:06:58 +0800

    公式平衡性调整

--- src/main/java/com/example/examplemod/Config.java
+++ src/main/java/com/example/examplemod/Config.java
@@ -21,21 +21,21 @@ public class Config {
             .comment("长期食用历史长度,用于统计长期饮食数据")
             .defineInRange("historyLengthLong", 512, 0, 2048);
 
-    public static final ModConfigSpec.ConfigValue<Integer> HISTORY_LENGTH_RECENT = BUILDER
+    public static final ModConfigSpec.ConfigValue<Integer> HISTORY_LENGTH_SHORT = BUILDER
             .comment("短期食用历史长度,用于统计短期饮食数据")
-            .defineInRange("historyLengthRecent", 16, 0, 2048);
+            .defineInRange("historyLengthSHORT", 16, 0, 2048);
 
     public static final ModConfigSpec.ConfigValue<String> LOSS = BUILDER
-            .comment("默认流失公式,取决于饥饿程度、近期营养水平、近期饮食 || 相关变量 ")
-            .define("loss", "0.01*Math.pow(2.0,(HUNGER_LEVEL-20.0)/10.0)*Math.pow(0.5,SATURATION_RECENT/Math.max(HUNGER_RECENT,1.0)-1.0)*Math.pow(32.0,(HUNGER_RECENT+SATURATION_RECENT-180.0)/640.0)");
+            .comment("默认自然流失公式，影响顺序：饥饿程度、短期营养水平、短期饮食水平、饱和程度 ||| 相关变量 玩家饱食度 HUNGER_LEVEL 短期总饱和度 SATURATION_SHORT 短期总饱食度 HUNGER_SHORT 玩家饱和度 SATURATION")
+            .define("loss", "0.01*Math.pow(2.0,(HUNGER_LEVEL-20.0)/10.0)*Math.pow(0.5,SATURATION_SHORT/Math.max(HUNGER_SHORT,1.0)-1.0)*Math.pow(2.0,(HUNGER_SHORT+SATURATION_SHORT-128.0)/128.0)+0.01*(Math.pow(2.0,(Math.min(SATURATION,40)-16.0)/4.0)-0.0625)");
 
     public static final ModConfigSpec.ConfigValue<String> HUNGER = BUILDER
-        .comment("默认饱食度公式,取决于近期饮食、长期饮食、饥饿程度 || 相关变量 该食物饱食度 HUNGER 该食物近期食用次数 EATEN_RECENT 玩家饱食度 HUNGER_LEVEL 该食物长期食用次数 EATEN_LONG")
-        .define("hunger", "HUNGER*0.4*Math.max(Math.pow(0.7,EATEN_RECENT),Math.max(1-HUNGER_LEVEL/10,0))+HUNGER*0.4*Math.max(1-EATEN_LONG/64,0)+HUNGER*0.2");
+        .comment("默认饱食度公式，影响顺序：短期饮食、饥饿程度、长期饮食 ||| 相关变量 该食物饱食度 HUNGER 该食物短期食用次数 EATEN_SHORT 玩家饱食度 HUNGER_LEVEL 该食物长期食用次数 EATEN_LONG")
+        .define("hunger", "HUNGER*0.4*Math.max(Math.pow(0.8,EATEN_SHORT),Math.max(1-HUNGER_LEVEL/12.0,0.0))+HUNGER*0.4*Math.max(1-EATEN_LONG/64.0,0.0)+HUNGER*0.2");
 
     public static final ModConfigSpec.ConfigValue<String> SATURATION = BUILDER
-        .comment("默认饱和度公式,取决于近期饮食、长期饮食、饥饿程度 || 相关变量 该食物饱和度 SATURATION 该食物近期食用次数 EATEN_RECENT 该食物长期食用次数 EATEN_LONG 玩家饱食度 HUNGER_LEVEL")
-        .define("saturation", "SATURATION*Math.pow(0.7,EATEN_RECENT)*Math.max(1.0-EATEN_LONG/64.0,0.0)+HUNGER*0.3*Math.max(1.0-HUNGER_LEVEL/10.0,0.0)");
+        .comment("默认饱和度公式,影响顺序：短期饮食、长期饮食、饥饿程度 ||| 相关变量 该食物饱和度 SATURATION 该食物短期食用次数 EATEN_SHORT 该食物长期食用次数 EATEN_LONG 玩家饱食度 HUNGER_LEVEL")
+        .define("saturation", "SATURATION*Math.pow(0.9,EATEN_SHORT)*Math.max(1.0-EATEN_LONG/64.0,0.0)+(HUNGER*0.2+SATURATION*0.2)*Math.max(1.0-HUNGER_LEVEL/12.0,0.0)");
 
     // a list of strings that are treated as resource locations for items
     public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
